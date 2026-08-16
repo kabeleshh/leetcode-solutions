8b3ea0105403660edc18884ac426a2b0f55fbbee
kabelesh <141481834+kabeleshh@users.noreply.github.com>
2026-08-16 16:06:04 +0530

    Time: 3805 ms (22.22%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,8 @@
+select 'Low Salary' as category, count(*) as accounts_count from Accounts
+where income < 20000
+union
+select 'Average Salary', count(*) from Accounts
+where income between 20000 and 50000
+union
+select 'High Salary', count(*) from Accounts
+where income > 50000;
