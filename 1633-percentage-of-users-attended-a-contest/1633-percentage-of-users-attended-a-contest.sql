e806ee544e13d0c949731e2e369bf5335c82a8c5
kabelesh <141481834+kabeleshh@users.noreply.github.com>
2026-09-20 10:30:40 +0530

    Time: 764 ms (99.28%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,2 @@
+select contest_id, round((count(user_id)::numeric/(select count(distinct user_id) from Users))*100,2) as percentage from Register group by contest_id
+order by percentage desc, contest_id asc;
